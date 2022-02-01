0c0a9b15481486270d7bb70052342b0c0b155e4b
Basic Java Coder <43179251+basicJavaCoder@users.noreply.github.com>
2022-02-01 10:19:34 +0000

    TemporaryCommit
    To save Script files that are mostly working.

---
+++ Scripts/IInteractable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IInteractable
+{
+    GameObject gameObject { get; }
+
+    void youveBeenTouched();
+
+    void moveTo(Vector3 destination);
+
+    void scale(float percentageChange);
+
+    void rotateObject(Vector3 v);
+
+    void objectSelected();
+
+    void objectDeselected();
+}
